b780713e41565ac802e30a1271e5144261563fea
Maximilien Garenne <maximilien@whatson-web.com>
2018-11-15 11:09:41 +0100

    [integration] add boilerplate component

---
+++ examples/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const ExampleClass = ({ blabla }) => <div>bliii</div>;
+
+ExampleClass.propTypes = {
+	blabla: PropTypes.string,
+};
+
+export default ExampleClass;
