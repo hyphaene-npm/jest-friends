263d8487b94e5b54ff2cfbc8cd2d8bae93eb9815
Maximilien Garenne <maximilien@whatson-web.com>
2018-11-15 10:14:58 +0100

    [integration] fix react parsing configuration

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import { mainInput } from './rollup/constants';
 import { setConfig, formatBuilder } from './rollup/utils';
 
 const jsExt = formatBuilder('umd');
-const mainOutput = [jsExt(pkg.browser)];
+const mainOutput = [jsExt(pkg.main)];
 const mainConfig = setConfig(mainInput, mainOutput);
 const config = [mainConfig];
 
